cd2c3125b1d6b0359402cad7ac50a92cb3bd91cb
sardor-abdullaev <abdullaevss94@mail.ru>
2025-07-13 18:56:44 +0500

    add signup and signin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,15 +15,21 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(private userService: UsersService, private authService: AuthService) { }
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    return this.userService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password)
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password)
   }
 
   //   @UseInterceptors(new SerializeInterceptor(UserDto))
